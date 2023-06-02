1eecc27658be550fbce9291082bd3220a921f889
BobBlank12 <39975390+BobBlank12@users.noreply.github.com>
2023-06-01 22:15:51 -0500

    added key format detection

--- certlint.py
+++ certlint.py
@@ -8,41 +8,75 @@ import subprocess
 
 os.system("clear")
 
-filenames=["es01.p12","es01.der","es01.key","es01.pem","es01.p7b"]
+filenames=["es01.p12","es01.der","es01.pkcs1.key","es01.pem","es01.p7b", "es01.pkcs8.key"]
 for filename in filenames:
 
-    print (f"Dertermining the file type of: {filename}")
+    print (f"Determining the file type of: {filename}")
 
-    #Test for P12 (that has no password for the cert)
-    #print (f"Testing P12 format...")
-    p12_format = subprocess.call(["openssl", "pkcs12", "-info", "-in", filename, "-nokeys", "-passin", "pass:"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=False, timeout=3)
-    print (f"p12_format={p12_format}")
+    # Order matters here since P12 files also show valid keys and DER formats
+    # so P12 HAS to be last in this check.
 
     #Test for PEM format
     #print (f"Testing PEM format...")
-    pem_format = subprocess.call(["openssl", "x509", "-inform", "pem", "-in", filename, "-text", "-noout"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=False, timeout=3)
-    print (f"pem_format={pem_format}")
+    result = subprocess.call(["openssl", "x509", "-inform", "pem", "-in", filename, "-text", "-noout"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=False, timeout=3)
+    if result == 0:
+        format = "pem"
 
     #Test for P7B format
     #print (f"Testing P7B format...")
-    p7b_format = subprocess.call(["openssl", "pkcs7", "-in", filename, "-print_certs"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=False, timeout=3)
-    print (f"p7b_format={p7b_format}")
+    result = subprocess.call(["openssl", "pkcs7", "-in", filename, "-print_certs"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=False, timeout=3)
+    if result == 0:
+        format = "p7b"
 
     #Test for DER format
     #print (f"Testing DER format...")
-    der_format = subprocess.call(["openssl", "x509", "-inform", "DER", "-in", filename, "-text", "-noout"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=False, timeout=3)
-    print (f"der_format={der_format}")
+    result = subprocess.call(["openssl", "x509", "-inform", "DER", "-in", filename, "-text", "-noout"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=False, timeout=3)
+    if result == 0:
+        format = "der"
 
     #Test to see if it is a key, not a cert
     #print (f"Testing if it is a key...")
-    key_format = subprocess.call(["openssl", "rsa", "-in", filename, "-check"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=False, timeout=3)
-    print (f"key_format={key_format}")
- 
-    # NEED TO ADD A TEST HERE TO SEE WHAT FORMAT THE KEY IS IN...
+    result= subprocess.call(["openssl", "rsa", "-in", filename, "-check"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=False, timeout=3)
+    if result == 0:
+        format = "key"
+
+    #Test for P12 (that has no password for the cert)
+    #print (f"Testing P12 format...")
+    result = subprocess.call(["openssl", "pkcs12", "-info", "-in", filename, "-nokeys", "-passin", "pass:"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=False, timeout=3)
+    if result == 0:
+        format = "p12"
+
+    # NEED TO ADD A TEST HERE TO SEE FOR ENCRYPTED PRIVATE KEYS
+    #-----BEGIN PRIVATE KEY----- = pkcs8
+    #-----BEGIN ENCRYPTED PRIVATE KEY----- pkcs8 encrypted
+    #-----BEGIN RSA PRIVATE KEY----- = pkcs1
 
     # ??? Need to add a test for PFX?  If PFX doesn't store certs in DER format?
 
     #  JKS files?
 
-    
+    print (f"\tIt appears you've given me a file that is in {format} format.")
+
+    if format == "key":
+        with open(filename) as f:
+            contents = f.readlines()
+        key_type = "unknown"
+        for line in contents:
+            result= line.find("-----BEGIN PRIVATE KEY-----")
+            if result != -1:
+                key_type = "pkcs8"
+                break
+            result = line.find("-----BEGIN ENCRYPTED PRIVATE KEY-----")
+            if result != -1:
+                key_type = "pkcs8-encrypted"
+                break
+            result= line.find("-----BEGIN RSA PRIVATE KEY-----")
+            if result != -1:
+                key_type = "pkcs1"
+                break
+        # End of for loop through key file
+        print (f"\tThe key file appears to be in {key_type} format.")
+    # End of key file checks
+    print (f"")
+# End of for loop through file list
 
