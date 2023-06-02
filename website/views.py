cf57a0c9ad049531e108c46b932f6c2c814f7956
BobBlank12 <39975390+BobBlank12@users.noreply.github.com>
2023-06-02 12:25:47 -0500

    This time a working simple file upload

--- website/views.py
+++ website/views.py
@@ -1,24 +1,16 @@
-from flask import Blueprint, render_template, request, flash, jsonify
-import json
+from flask import Blueprint, render_template, request, flash
 
 views = Blueprint('views', __name__)
-
 
 @views.route('/', methods=['GET', 'POST'])
 def home():
     # if request.method == 'POST': 
+    return render_template("index.html")
 
-    return render_template("home.html")
+@views.route('/success', methods = ['POST'])  
+def success():  
+    if request.method == 'POST':  
+        f = request.files['file']
+        f.save(f.filename)  
+        return render_template("uploadsuccess.html", name = f.filename)
 
-
-@views.route('/delete-note', methods=['POST'])
-def delete_note():  
-    note = json.loads(request.data) # this function expects a JSON from the INDEX.js file 
-    noteId = note['noteId']
-    note = Note.query.get(noteId)
-    if note:
-        if note.user_id == current_user.id:
-            db.session.delete(note)
-            db.session.commit()
-
-    return jsonify({})
