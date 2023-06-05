d2b3c523b9599207634e5b767e6517d548c91086
BobBlank12 <39975390+BobBlank12@users.noreply.github.com>
2023-06-05 18:41:47 -0500

    cerlint.py is now a file of functions imported in views.py

--- website/views.py
+++ website/views.py
@@ -1,80 +1,8 @@
 from flask import Blueprint, render_template, request, flash
 import os
+import sys
 import subprocess
-
-def determine_cert_type(filename, fileformat, key_format):
-    #print (f"Determining the file type of: {filename}")
-
-    # Order matters here since P12 files also show valid keys and DER formats
-    # so P12 HAS to be last in this check.
-
-    #Test for PEM format
-    #print (f"Testing PEM format...")
-    result = subprocess.call(["openssl", "x509", "-inform", "pem", "-in", filename, "-text", "-noout"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=False, timeout=3)
-    if result == 0:
-        fileformat = "pem"
-
-    #Test for P7B format
-    #print (f"Testing P7B format...")
-    result = subprocess.call(["openssl", "pkcs7", "-in", filename, "-print_certs"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=False, timeout=3)
-    if result == 0:
-        fileformat = "p7b"
-
-    #Test for DER format
-    #print (f"Testing DER format...")
-    result = subprocess.call(["openssl", "x509", "-inform", "DER", "-in", filename, "-text", "-noout"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=False, timeout=3)
-    if result == 0:
-        fileformat = "der"
-
-    #Test to see if it is a key, not a cert (that has no password for the key)
-    #print (f"Testing if it is a key...")
-    result= subprocess.call(["openssl", "rsa", "-in", filename, "-check", "-passin", "pass:"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=False, timeout=3)
-    if result == 0:
-        fileformat = "key"
-
-
-    #Test for P12 (that has no password for the cert)
-    #print (f"Testing P12 format...")
-    result = subprocess.call(["openssl", "pkcs12", "-info", "-in", filename, "-nokeys", "-passin", "pass:"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=False, timeout=3)
-    if result == 0:
-        fileformat = "p12"
-
-    # Notes on keys...
-    #-----BEGIN PRIVATE KEY----- = pkcs8
-    #-----BEGIN ENCRYPTED PRIVATE KEY----- pkcs8 encrypted
-    #-----BEGIN RSA PRIVATE KEY----- = pkcs1
-
-    # ??? Need to add a test for PFX?  If PFX doesn't store certs in DER format?
-
-    #  JKS files?
-
-    #print (f"\tIt appears you've given me a file that is in {format} format.")
-
-    if fileformat == "key":
-        with open(filename) as f:
-            contents = f.readlines()
-        key_format = "unknown"
-        for line in contents:
-            result= line.find("-----BEGIN PRIVATE KEY-----")
-            if result != -1:
-                key_format = "pkcs8"
-                break
-            result = line.find("-----BEGIN ENCRYPTED PRIVATE KEY-----")
-            if result != -1:
-                key_format = "pkcs8-encrypted"
-                break
-            result= line.find("-----BEGIN RSA PRIVATE KEY-----")
-            if result != -1:
-                key_format = "pkcs1"
-                break
-            result= line.find("-----BEGIN ENCRYPTED PRIVATE KEY-----")
-            if result != -1:
-                key_format = "pkcs8-encrypted"
-                break
-        # End of for loop through key file
-        #print (f"\tThe key file appears to be in {key_format} format.")
-    # End of key file checks
-# End of determine_cert_type
+from certlint import *
 
 views = Blueprint('views', __name__)
 
@@ -90,74 +18,43 @@ def success():
         certfile = f.filename
         f.save(f.filename)
         f.close
-        key_format = "n/a"
         fileformat = "unknown"
+        key_type = "n/a"
 
-        #print (f"Determining the file type of: {filename}")
+        # Determine the file format I've been given
+        fileformat, key_type = determine_file_format(f.filename, fileformat, key_type)
 
-        # Order matters here since P12 files also show valid keys and DER formats
-        # so P12 HAS to be last in this check.
+        # Convert to PEM from whatever I've received
+        if fileformat == "der":
+            try:
+                convert_der_to_pem(f.filename)
+            except Exception as e:
+                print("Error converting DER to PEM {:}".format(e))
+            else:
+                print(f"\tI've created a {f.filename}-converted.pem file for you.")
 
-        #Test for PEM format
-        #print (f"Testing PEM format...")
-        result = subprocess.call(["openssl", "x509", "-inform", "pem", "-in", certfile, "-text", "-noout"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=False, timeout=3)
-        if result == 0:
-            fileformat = "pem"
+        if fileformat == "p12":
+            try:
+                convert_p12_to_pem(f.filename)
+            except Exception as e:
+                print("Error converting P12 to PEM {:}".format(e))
+            else:
+                print(f"\tI've created a {filename}-converted.pem file for you.")
 
-        #Test for P7B format
-        #print (f"Testing P7B format...")
-        result = subprocess.call(["openssl", "pkcs7", "-in", certfile, "-print_certs"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=False, timeout=3)
-        if result == 0:
-            fileformat = "p7b"
+        if fileformat == "p7b":
+            try:
+                convert_p7b_to_pem(f.filename)
+            except Exception as e:
+                print("Error converting P7B to PEM {:}".format(e))
+            else:
+                print(f"\tI've created a {f.filename}-converted.pem file for you.")
 
-        #Test for DER format
-        #print (f"Testing DER format...")
-        result = subprocess.call(["openssl", "x509", "-inform", "DER", "-in", certfile, "-text", "-noout"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=False, timeout=3)
-        if result == 0:
-            fileformat = "der"
+        if fileformat == "p7b":
+            try:
+                convert_p7b_to_pem(f.filename)
+            except Exception as e:
+                print("Error converting P7B to PEM {:}".format(e))
+            else:
+                print(f"\tI've created a {f.filename}-converted.pem file for you.")
 
-        #Test to see if it is a key, not a cert (that has no password for the key)
-        #print (f"Testing if it is a key...")
-        result= subprocess.call(["openssl", "rsa", "-in", certfile, "-check", "-passin", "pass:"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=False, timeout=3)
-        if result == 0:
-            fileformat = "key"
-
-        #Test for P12 (that has no password for the cert)
-        #print (f"Testing P12 format...")
-        result = subprocess.call(["openssl", "pkcs12", "-info", "-in", certfile, "-nokeys", "-passin", "pass:"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=False, timeout=3)
-        if result == 0:
-            fileformat = "p12"
-
-        # Notes on keys...
-        #-----BEGIN PRIVATE KEY----- = pkcs8
-        #-----BEGIN ENCRYPTED PRIVATE KEY----- pkcs8 encrypted
-        #-----BEGIN RSA PRIVATE KEY----- = pkcs1
-
-        # ??? Need to add a test for PFX?  If PFX doesn't store certs in DER format?
-        #  JKS files?
-
-        #print (f"\tIt appears you've given me a file that is in {format} format.")
-
-        if fileformat == "key":
-            with open(f.filename) as f:
-                contents = f.readlines()
-            for line in contents:
-                result= line.find("-----BEGIN PRIVATE KEY-----")
-                if result != -1:
-                    key_format = "pkcs8"
-                    break
-                result = line.find("-----BEGIN ENCRYPTED PRIVATE KEY-----")
-                if result != -1:
-                    key_format = "pkcs8-encrypted"
-                    break
-                result= line.find("-----BEGIN RSA PRIVATE KEY-----")
-                if result != -1:
-                    key_format = "pkcs1"
-                    break
-
-            # End of for loop through key file
-            #print (f"\tThe key file appears to be in {key_format} format.")
-        # End of key file checks
-
-        return render_template("uploadsuccess.html", name = certfile, fileformat = fileformat, key_format = key_format)
-
+        return render_template("uploadsuccess.html", name = certfile, fileformat = fileformat, key_type = key_type)
