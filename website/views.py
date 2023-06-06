d9950e1515d1c8eb2880ac582750c1f09a751a19
BobBlank12 <39975390+BobBlank12@users.noreply.github.com>
2023-06-06 16:52:15 -0500

    added links to download certs converted to pem

--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, url_for, send_file
 import os
 import sys
 import subprocess
@@ -11,11 +11,14 @@ def home():
     # if request.method == 'POST': 
     return render_template("index.html")
 
+@views.route("/<filename>-converted-to.pem", methods=['GET', 'POST'])
+def getFile(filename):
+    return send_file("../"+filename+"-converted-to.pem", as_attachment=True)
+
 @views.route('/success', methods = ['POST'])  
 def success():  
     if request.method == 'POST':  
         f = request.files['file']
-        certfile = f.filename
         f.save(f.filename)
         f.close
         fileformat = "unknown"
@@ -24,14 +27,32 @@ def success():
         # Determine the file format I've been given
         fileformat, key_type = determine_file_format(f.filename, fileformat, key_type)
 
+        #print(f"fileformat={fileformat}")
+
         # Convert to PEM from whatever I've received
+
+        links = []
+
+        links.append(
+            {
+                "Original file: " : f.filename,
+                "Original file format:" : fileformat,
+                "Original key format:" : key_type
+            }
+        )
+
         if fileformat == "der":
             try:
                 convert_der_to_pem(f.filename)
             except Exception as e:
                 print("Error converting DER to PEM {:}".format(e))
             else:
-                print(f"\tI've created a {f.filename}-converted.pem file for you.")
+                print(f"\tI've created a {f.filename}-converted-to.pem file for you.")
+                links.append(
+                    {
+                        f.filename+"-converted-to.pem:" : '<a href="'+f.filename+'-converted-to.pem">'+f.filename+'-converted-to.pem</a>'
+                    }
+                )
 
         if fileformat == "p12":
             try:
@@ -39,7 +60,12 @@ def success():
             except Exception as e:
                 print("Error converting P12 to PEM {:}".format(e))
             else:
-                print(f"\tI've created a {filename}-converted.pem file for you.")
+                print(f"\tI've created a {f.filename}-converted-to.pem file for you.")
+                links.append(
+                    {
+                        f.filename+"-converted-to.pem:" : '<a href="'+f.filename+'-converted-to.pem">'+f.filename+'-converted-to.pem</a>'
+                    }
+                )
 
         if fileformat == "p7b":
             try:
@@ -48,6 +74,11 @@ def success():
                 print("Error converting P7B to PEM {:}".format(e))
             else:
                 print(f"\tI've created a {f.filename}-converted.pem file for you.")
+                links.append(
+                    {
+                        f.filename+"-converted-to.pem:" : '<a href="'+f.filename+'-converted-to.pem">'+f.filename+'-converted-to.pem</a>'
+                    }
+                )
 
         if fileformat == "p7b":
             try:
@@ -56,5 +87,10 @@ def success():
                 print("Error converting P7B to PEM {:}".format(e))
             else:
                 print(f"\tI've created a {f.filename}-converted.pem file for you.")
-
-        return render_template("uploadsuccess.html", name = certfile, fileformat = fileformat, key_type = key_type)
+                links.append(
+                    {
+                        f.filename+"-converted-to.pem:" : '<a href="'+f.filename+'-converted-to.pem">'+f.filename+'-converted-to.pem</a>'
+                        
+                    }
+                )
+        return render_template("uploadsuccess.html", links=links)
