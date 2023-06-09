71a77e7d89b84bca931eb7aba3a8d4c1176e676e
BobBlank12 <39975390+BobBlank12@users.noreply.github.com>
2023-06-09 11:28:56 -0500

    some error checking and web cleanup

--- website/views.py
+++ website/views.py
@@ -52,9 +52,11 @@ def success():
 
         # Convert to PEM from whatever I've received
 
+        orig = []
         links = []
+        certdetails = []
 
-        links.append(
+        orig.append(
             {
                 "Original file: " : f.filename,
                 "Original file format:" : fileformat,
@@ -143,20 +145,21 @@ def success():
                     )
         # End if fileformat = p7b
 
-        # At this point I should have a clean PEM file I can create all of the other formats from...
-        try:
-            convert_pem_to_der("website/" + UPLOAD_FOLDER+f.filename)
-        except Exception as e:
-            print("Error converting PEM to DER {:}".format(e))
-        else:
-            #print(f"\tI've created a {f.filename}-converted-to.der file for you.")
-            links.append(
-                {
-                    f.filename+"-converted-to.der:" : '<a href="'+UPLOAD_FOLDER+f.filename+'-converted-to.der">'+f.filename+'-converted-to.der</a>'
-                }
-            )
+        if fileformat != "key":
+            # At this point I should have a clean PEM file I can create all of the other formats from...
+            try:
+                convert_pem_to_der("website/" + UPLOAD_FOLDER+f.filename)
+            except Exception as e:
+                print("Error converting PEM to DER {:}".format(e))
+            else:
+                #print(f"\tI've created a {f.filename}-converted-to.der file for you.")
+                links.append(
+                    {
+                        f.filename+"-converted-to.der:" : '<a href="'+UPLOAD_FOLDER+f.filename+'-converted-to.der">'+f.filename+'-converted-to.der</a>'
+                    }
+                )
+            # Get the cert details from the converted pem file
+            certdetails = get_pem_cert_details("website/" + UPLOAD_FOLDER + f.filename+"-converted-to.pem")
+        # End if fileformat != key
 
-        # Get the cert details from the converted pem file
-        certdetails = get_pem_cert_details("website/" + UPLOAD_FOLDER + f.filename+"-converted-to.pem")
-
-        return render_template("uploadsuccess.html", links=links, certdetails=certdetails)
+        return render_template("uploadsuccess.html", orig=orig, links=links, certdetails=certdetails)
